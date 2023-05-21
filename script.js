203a7bc7194b40dd30fac358f58c50495300eaed
Shubham <sc409705@gmail.com>
2023-05-21 23:01:32 +0530

    Added Extra Progames

---
+++ script.js
@@ -0,0 +1,11 @@
+var char = document.body.addEventListener('keypress', function(e)
+{
+
+    var p1 = document.getElementById("asciichar");
+    var p2 = document.getElementById("asciivalue");
+
+    p1.innerHTML = e.key
+    p2.innerHTML = e.keyCode;
+
+
+})
